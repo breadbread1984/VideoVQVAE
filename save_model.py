9a0018f965c36d416329bfbb771762ca60a900f6
root <root@LAPTOP-A2EG90UI.localdomain>
2021-07-20 23:17:38 +0800

    update

--- save_model.py
+++ save_model.py
@@ -14,25 +14,14 @@ def main(unused_argv):
   if not exists('models'): mkdir('models');
   trainer = VideoVQVAE_Trainer(use_2d = FLAGS.use_2d);
   trainer.load_weights('./checkpoints/ckpt');
-  trainer.save(join('models', 'trainer.h5'));
-  trainer.layers[1].save(join('models', 'encoder.h5'));
-  trainer.layers[2].save_weights(join('models', 'pre_vq_conv.h5'));
-  trainer.layers[4].save_weights(join('models', 'post_vq_conv.h5'));
-  trainer.layers[5].save(join('models', 'decoder.h5'));
-  video_vqvae = VideoVQVAE(use_2d = FLAGS.use_2d);
-  # predict once to initialize subclass model's parameters
-  inputs = np.random.normal(size = (4, 16, 64, 64, 3));
-  outputs = video_vqvae(inputs);
-  video_vqvae.encoder = tf.keras.models.load_model(join('models', 'encoder.h5'));
-  video_vqvae.decoder = tf.keras.models.load_model(join('models', 'decoder.h5'));
-  video_vqvae.pre_vq_conv.load_weights(join('models', 'pre_vq_conv.h5'));
-  video_vqvae.post_vq_conv.load_weights(join('models', 'post_vq_conv.h5'));
-  video_vqvae.codebook.cluster_mean.assign(trainer.layers[3].cluster_mean);
-  video_vqvae.codebook.cluster_size.assign(trainer.layers[3].cluster_size);
-  video_vqvae.codebook.cluster_sum.assign(trainer.layers[3].cluster_sum);
-  video_vqvae.codebook.initialized = True;
-  video_vqvae.codebook.enable_train = False;
-  video_vqvae.save_weights(join('models', 'video_vqvae_weights.h5'));
+  trainer.layers[3].set_trainable(False);
+  encoder = tf.keras.Model(inputs = trainer.layers[0].input, outputs = trainer.layers[3].output);
+  encoder.save(join('models', 'encoder.h5'));
+  quantized = tf.keras.Input((None, None, None, 256));
+  results = trainer.layers[4](quantized);
+  recon = trainer.layers[5](results);
+  decoder = tf.keras.Model(inputs = quantized, outputs = recon);
+  decoder.save(join('models', 'decoder.h5'));
 
 if __name__ == "__main__":
 
